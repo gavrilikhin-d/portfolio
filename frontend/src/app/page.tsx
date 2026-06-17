99033bce52bce552d78e2fbc7d51de61eabca414
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2026-06-18 02:03:17 +0300

    add background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full items-center px-8">
+    <div className="flex min-h-screen w-full items-center overflow-hidden bg-black bg-[url('/background.webp')] bg-cover bg-center bg-no-repeat px-8">
       <div className="mx-auto flex w-full max-w-5xl flex-col items-start">
         <div>
           <p className="text-gradient text-xl font-medium pl-[1.5px]"> {/* pl-[1.5px] is an optical adjustment to make `H` align with `F` */}
