57047323c4543ba6683a68b4a350bd767124679e
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2026-06-20 14:49:35 +0300

    hero-appear animation

--- frontend/src/components/sections/hero/index.tsx
+++ frontend/src/components/sections/hero/index.tsx
@@ -1,7 +1,9 @@
+import "./styles.css";
+
 export default function Hero() {
   return (
     <div className="flex min-h-screen w-full items-center overflow-hidden bg-black bg-[url('/background.webp')] bg-cover bg-center bg-no-repeat px-8">
-      <div className="mx-auto flex w-full max-w-5xl flex-col items-start">
+      <div className="hero-appear mx-auto flex w-full max-w-5xl flex-col items-start">
         <div>
           <p className="text-gradient-primary text-xl font-medium pl-[1.5px]"> {/* pl-[1.5px] is an optical adjustment to make `H` align with `F` */}
             Hello, I'm Daniil
